9e9e4dc625a619fff76938a57c1dd07f10018c62
EthanNielsen <31518851+EthanNielsen@users.noreply.github.com>
2017-10-26 12:43:08 -0600

    added the 7 big ideas as swift files.

---
+++ EthanN_CSP/AlgorithmsController.swift
@@ -0,0 +1,9 @@
+//
+//  AlgorithmsController.swift
+//  EthanN_CSP
+//
+//  Created by Nielsen, Ethan on 10/26/17.
+//  Copyright © 2017 Ctech. All rights reserved.
+//
+
+import Foundation
---
+++ EthanN_CSP/ImpactController.swift
@@ -0,0 +1,9 @@
+//
+//  ImpactController.swift
+//  EthanN_CSP
+//
+//  Created by Nielsen, Ethan on 10/26/17.
+//  Copyright © 2017 Ctech. All rights reserved.
+//
+
+import Foundation
