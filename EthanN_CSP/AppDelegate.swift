934868374eccdbd8f90dc2b56e814c3450b44bfa
EthanNielsen <31518851+EthanNielsen@users.noreply.github.com>
2017-12-01 13:58:14 -0700

    dding code to the AppDelgate.

--- EthanN_CSP/AppDelegate.swift
+++ EthanN_CSP/AppDelegate.swift
@@ -38,7 +38,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
 
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        // If your application supports background execution, this method is called instead of applicationWillTerminAate: when the user quits.
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
@@ -53,6 +53,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
-
+    func splitViewController(_ splitViewController: UISplitViewController,
+                             collapseSecondary secondaryViewController : UIViewController,
+                             onto primaryViewController : UIViewController : UIViewController) -> Bool
+    {
+        guard let secondaryAsNavController =
+            secondaryViewController as? UINavigationController
+        
+    }
+    
 }
 
