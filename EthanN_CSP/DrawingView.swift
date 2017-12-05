ee034e13b2be164d7186e266e8e7f861527aa4f9
EthanNielsen <31518851+EthanNielsen@users.noreply.github.com>
2017-12-05 13:22:52 -0700

    I am drawing with a picture.

--- EthanN_CSP/DrawingView.swift
+++ EthanN_CSP/DrawingView.swift
@@ -44,7 +44,27 @@ class DrawingView: UIView
     
     public func drawHappyTree() -> Void
     {
+        let HotDog = UIBezierPath()
         
+        HotDog.move(to: CGPoint(x: 80, y: 50))
+        HotDog.addLine(to: CGPoint(x: 120, y: 150))
+        HotDog.addLine(to: CGPoint(x: 40, y: 150))
+        HotDog.close()
+        
+        let happyTree = UIBezierPath()
+        
+        UIColor.green.setFill()
+        happyTree.move(to: CGPoint(x: 110, y: 150))
+        happyTree.addLine(to: CGPoint(x: 150, y: 200))
+        happyTree.addLine(to: CGPoint(x: 10, y: 200))
+        happyTree.addLine(to: CGPoint(x: 50, y: 150))
+        happyTree.close()
+        happyTree.stroke()
+        happyTree.fill()
+        happyTree.move(to: CGPoint(x: 80, y: 200))
+        happyTree.addLine(to: CGPoint(x: 80, y: 250))
+        happyTree.lineWidth = 6.0
+        happyTree.stroke()
     }
     
     private func drawTurtle() -> Void
