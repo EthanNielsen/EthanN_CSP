9745f2282c56ff11adb052a9473d7d0c93134474
EthanNielsen <31518851+EthanNielsen@users.noreply.github.com>
2017-12-11 11:56:55 -0700

    Added code to the CreativityViewController and fixed a lot of the typoes that I had in the code.

--- EthanN_CSP/CreativityViewController.swift
+++ EthanN_CSP/CreativityViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-public class CreativityViewController : UICollectionViewCell, UICollectionViewDelegateFlowLayout
+public class CreativityViewController : UICollectionViewController, UICollectionViewDelegateFlowLayout
 {
     private let reuseIdentifier = "artIdentifier"
     private let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
@@ -71,19 +71,19 @@ public class CreativityViewController : UICollectionViewCell, UICollectionViewDe
     
     //MARK:- UICollectionView methods
     
-    override public func numberOfSelections(in collectionView: UICollectionView) -> Int
+    override public func numberOfSections(in collectionView: UICollectionView) -> Int
     {
         return 1
     }
     
-    override public func collectionView(_ collectionView: UICollecctionView,
+    override public func collectionView(_ collectionView: UICollectionView,
                                         numberOfItemsInSection section: Int) -> Int
     {
         return artSelection.count
     }
     
     override public func collectionView(_ collectionView: UICollectionView,
-                                        cellForItemAt indexPath: IndexPath) -> UICollectionView
+                                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell
     {
         let artCell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! ArtCell
         
@@ -93,13 +93,55 @@ public class CreativityViewController : UICollectionViewCell, UICollectionViewDe
         
         return artCell
     }
-    
+
     //MARK:- Delegate Methods
     
-    public 
+    public func collectionView(_ collectionView: UICollectionView,
+                               layout collectionViewLayout: UICollectionViewLayout,
+                               sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        if indexPath == largePhotoIndexPath
+        {
+            let art = artSelection[indexPath.row]
+            let size = collectionView.bounds.size
+            let widthScale = (size.width / art!.size.width) * CGFloat(0.80)
+            let largeSize = CGSize(width: art!.size.width * widthScale, height: art!.size.height * widthScale)
+            
+            return largeSize
+        }
+        
+        let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
+        let availableWidth = view.frame.width - paddingSpace
+        let widthPerItem = availableWidth / itemsPerRow
+        
+        return CGSize(width: widthPerItem, height: widthPerItem)
+    }
     
+    public func collectionView(_ collectionView: UICollectionView,
+                               layout collectionViewLayout: UICollectionViewLayout,
+                               insetForSectionAt section: Int) -> UIEdgeInsets
+    {
+        return sectionInsets
+    }
     
+    public func collectionView(_ collectionView: UICollectionView,
+                               layout collectionViewLayout: UICollectionViewLayout,
+                               minimumLineSpacingForSectionAt section: Int) -> CGFloat
+    {
+        return sectionInsets.left
+    }
     
-    
-    
+    override public func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool
+    {
+        if largePhotoIndexPath == indexPath
+        {
+            largePhotoIndexPath = nil
+        }
+        else
+        {
+            largePhotoIndexPath = indexPath
+        }
+        
+        return false
+    }
 }
