89222000411b8bc7af141810e3c7bbe4adaef6e0
EthanNielsen <31518851+EthanNielsen@users.noreply.github.com>
2017-12-19 13:51:40 -0700

    Added the death scene screen

--- EthanN_CSP/DeathScene.swift
+++ EthanN_CSP/DeathScene.swift
@@ -6,4 +6,35 @@
 //  Copyright © 2017 Ctech. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import SpriteKit
+
+public class DeathScene : SKScene
+{
+    override public func didMove(to view: SKView) -> Void
+    {
+        self.backgroundColor = SKColor.red
+        
+        let gameOver = SKLabelNode(text: "Keep the change you filthy animal!")
+        gameOver.name = "gamever"
+        gameOver.fontSize = 65
+        gameOver.fontColor = SKColor.white
+        gameOver.position = CGPoint(x: frame.midX, y: frame.midY)
+        
+        addChild(gameOver)
+    }
+    
+    override public func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) -> Void
+    {
+        let touch = touches.first
+        let touchLocation = touch?.location(in: self)
+        let touchedNode = self.atPoint(touchLocation!)
+        if(touchedNode.name == "game over")
+        {
+            let newGameScene = GameScene(size: size)
+            newGameScene.scaleMode = scaleMode
+            let transitionType = SKTransition.flipHorizontal(withDuration: 0.5)
+            view?.presentScene(newGameScene, transition: transitionType)
+        }
+    }
+}
